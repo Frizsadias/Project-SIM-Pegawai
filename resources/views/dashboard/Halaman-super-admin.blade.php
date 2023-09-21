c41e0de34675e6ab31f7044d2223a706b72d04b1
Kelvin <kelvin.p2504@gmail.com>
2023-09-21 20:00:12 +0700

    Pembaharuan

--- resources/views/dashboard/Halaman-super-admin.blade.php
+++ resources/views/dashboard/Halaman-super-admin.blade.php
@@ -46,31 +46,67 @@
                         </div>
                     </div>
                 </div>
+            </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="container px-4 mx-auto">
-                                    {!! $chart->container() !!}
-                                    {!! $grafikAgama->container() !!}
-                                    {!! $grafikJenisKelamin->container() !!}
-                                    {!! $grafikPangkat->container() !!}                
+                        <div class="row">
+                            <div class="col-md-6 text-center">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h3 class="card-title"><b>Riwayat Pendidikan</b></h3>
+                                        <div class="container px-4 mx-auto">
+                                            {!! $chart->container() !!}
+                                        </div>
+                                        <script src="{{ $chart->cdn() }}"></script>
+                                        {{ $chart->script() }}
+                                    </div>
                                 </div>
-                
-                                <script src="{{ $chart->cdn() }}"></script>
-                                <script src="{{ $grafikAgama->cdn() }}"></script>
-                                <script src="{{ $grafikJenisKelamin->cdn() }}"></script>
-                                <script src="{{ $grafikPangkat->cdn() }}"></script>
-                
-                                {{ $chart->script() }}
-                                {{ $grafikAgama->script() }}
-                                {{ $grafikJenisKelamin->script() }}
-                                {{ $grafikPangkat->script() }}
+                            </div>
+                            <div class="col-md-6 text-center">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h3 class="card-title"><b>Grafik Pegawai Berdasarkan Agama</b></h3>
+                                        <div class="container px-4 mx-auto">
+                                        {!! $grafikAgama->container() !!}
+                                        </div>
+                                        <script src="{{ $grafikAgama->cdn() }}"></script>
+                                        {{ $grafikAgama->script() }}
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-6 text-center">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h3 class="card-title"><b>Grafik Pegawai Berdasarkan Jenis Kelamin</b></h3>
+                                        <div class="container px-4 mx-auto">
+                                            {!! $grafikJenisKelamin->container() !!}
+                                        </div>
+                                        <script src="{{ $grafikJenisKelamin->cdn() }}"></script>
+                                        {{ $grafikJenisKelamin->script() }}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6 text-center">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h3 class="card-title">Jumlah Pegawai Berdasarkan Pangkat</h3>
+                                        <div class="container px-4 mx-auto">
+                                            {!! $grafikPangkat->container() !!}
+                                        </div>
+                                        <script src="{{ $grafikPangkat->cdn() }}"></script>
+                                        {{ $grafikPangkat->script() }}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             {{-- message --}}
             {!! Toastr::message() !!}
         </div>
