27e43cacaf357435a67ab2844ff0f51ced9005ac
Kelvin <kelvin.p2504@gmail.com>
2023-10-04 17:44:45 +0700

    Perbaiki kesalahan trigger

--- database/migrations/2023_10_04_170832_create_generate_user_id_posisijabatan_table.php
+++ database/migrations/2023_10_04_170832_create_generate_user_id_posisijabatan_table.php
@@ -14,13 +14,16 @@ class CreateGenerateUserIdPosisiJabatanTable extends Migration
     public function up()
     {
         DB::unprepared('
-            CREATE TRIGGER update_posisijabatan AFTER UPDATE ON posisi_jabatan FOR EACH ROW
+            CREATE TRIGGER update_profilpegawai AFTER UPDATE ON posisi_jabatan FOR EACH ROW
             BEGIN
-                UPDATE profil_pegawais SET unit_organisasi=NEW.unit_organisasi,unit_organisasi_induk=NEW.unit_organisasi_induk,
-                jenis_jabatan=NEW.jenis_jabatan,eselon=NEW.eselon,jabatan=NEW.jabatan,tmt=NEW.tmt,
-                lokasi_kerja=NEW.lokasi_kerja,gol_ruang_awal=NEW.gol_ruang_awal,gol_ruang_akhir=NEW.gol_ruang_akhir,
-                tmt_golongan=NEW.tmt_golongan,gaji_pokok=NEW.gaji_pokok,masa_kerja_tahun=NEW.masa_kerja_tahun,
-                masa_kerja_bulan=NEW.masa_kerja_bulan,no_spmt=NEW.no_spmt,tanggal_spmt=NEW.tanggal_spmt,kppn=NEW.kppn
+                UPDATE posisi_jabatans SET unit_organisasi=NEW.unit_organisasi,unit_organisasi_induk=NEW.unit_organisasi_induk,
+                jenis_jabatan=NEW.jenis_jabatan,eselon=NEW.eselon,
+                jabatan=NEW.jabatan,tmt=NEW.tmt,
+                lokasi_kerja=NEW.lokasi_kerja,gol_ruang_awal=NEW.gol_ruang_awal,
+                gol_ruang_akhir=NEW.gol_ruang_akhir,
+                tmt_golongan=NEW.tmt_golongan,gaji_pokok=NEW.gaji_pokok,
+                masa_kerja_tahun=NEW.masa_kerja_tahun,masa_kerja_bulan=NEW.masa_kerja_bulan,
+                no_spmt=NEW.no_spmt,tanggal_spmt=NEW.tanggal_spmt,kppn=NEW.kppn
             WHERE id=OLD.id;
             END
         ');
