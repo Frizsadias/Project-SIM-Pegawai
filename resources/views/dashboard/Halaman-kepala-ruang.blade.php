09cf6cb7f13d667f2a42f9371fb23d629bc0863c
Frizsadias <frizsadias20@gmail.com>
2023-11-10 23:27:39 +0700

    menambah grafik di kepala ruang

--- resources/views/dashboard/Halaman-kepala-ruang.blade.php
+++ resources/views/dashboard/Halaman-kepala-ruang.blade.php
@@ -773,6 +773,80 @@
             <div class="lihat-semua">
                 <button type="button" class="btn btn-info" id="lihatSemuaTempatTidur">Lihat Semua</button>
             </div><br>
+
+            <div class="col-md-12">
+                <div class="card dash-widget">
+                    <div class="card-body">
+                        <div class="dash-widget-info">
+                            <center><span style="font-size: 20px; font-weight: 600; font-family: Poppins;">Informasi
+                                    Grafik</span></center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-6 text-center">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h3 class="card-title"><b>Riwayat Pendidikan</b></h3>
+                                    <div class="container px-4 mx-auto">
+                                        {!! $chart->container() !!}
+                                    </div>
+                                    <script src="{{ $chart->cdn() }}"></script>
+                                    {{ $chart->script() }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 text-center">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h3 class="card-title"><b>Grafik Pegawai Berdasarkan Agama</b></h3>
+                                    <div class="container px-4 mx-auto">
+                                        {!! $grafikAgama->container() !!}
+                                    </div>
+                                    <script src="{{ $grafikAgama->cdn() }}"></script>
+                                    {{ $grafikAgama->script() }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-6 text-center">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h3 class="card-title"><b>Grafik Pegawai Berdasarkan Jenis Kelamin</b></h3>
+                                    <div class="container px-4 mx-auto">
+                                        {!! $grafikJenisKelamin->container() !!}
+                                    </div>
+                                    <script src="{{ $grafikJenisKelamin->cdn() }}"></script>
+                                    {{ $grafikJenisKelamin->script() }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 text-center">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h3 class="card-title"><b>Jumlah Pegawai Berdasarkan Pangkat</b></h3>
+                                    <div class="container px-4 mx-auto">
+                                        {!! $grafikPangkat->container() !!}
+                                    </div>
+                                    <script src="{{ $grafikPangkat->cdn() }}"></script>
+                                    {{ $grafikPangkat->script() }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- message --}}
+            {!! Toastr::message() !!}
         </div>
     </div>
 
